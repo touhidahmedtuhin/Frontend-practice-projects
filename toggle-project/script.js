8a5577a5c403ee8e5ccb5c4fffba452d32348268
Touhid Ahmed Tuhin <tuhin2668@gmail.com>
2025-07-19 09:57:44 +0600

    Add files via upload

---
+++ toggle-project/script.js
@@ -0,0 +1,9 @@
+function toggleProject() {
+    console.log('toggle');
+    const para = document.querySelector('#paradiv');
+    para.classList.toggle('hidden');
+}
+
+// const toggle = document.querySelector('.mypara');
+// console.log(toggle.classList)
+// console.log(toggle.className)
